406427bd10241deb1d81e90687d267a512e71d23
MariuszRozycki <mariansc83@icloud.com>
2024-03-11 18:39:18 +0100

    Add function getLatestsPosts to component Home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { baseApi } from "../api/baseApi";
+
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getLatestsPosts = () => {
+    const latestsPostsApi = baseApi + "post/latest";
+    axios
+      .post(latestsPostsApi)
+      .then((request) => {
+        setPosts(request.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getLatestsPosts();
+  }, []);
+
   return <h2>Home</h2>;
 };
 
