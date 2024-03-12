83943fad6279239723f635a3453ce64d8b1e1be0
MariuszRozycki <mariansc83@icloud.com>
2024-03-12 23:39:35 +0100

    Change class to className in Loader component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,8 +2,8 @@ import "./Loader.css";
 
 const Loader = () => {
   return (
-    <div class="loader-wrapper">
-      <div class="loader"></div>
+    <div className="loader-wrapper">
+      <div className="loader"></div>
     </div>
   );
 };
