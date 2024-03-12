4082a90fd6eb4020d677926d83205a01afb425c7
MariuszRozycki <mariansc83@icloud.com>
2024-03-12 12:20:59 +0100

    Add styles for .loadMore

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,19 +1,45 @@
+import "./Home.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { baseApi } from "../api/baseApi";
+import Post from "../components/Post";
+import Loader from "../components/Loader";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getLatestsPosts = () => {
+    setIsLoading(true);
     const latestsPostsApi = baseApi + "post/latest";
     axios
       .post(latestsPostsApi)
-      .then((request) => {
-        setPosts(request.data);
+      .then((response) => {
+        setPosts(response.data);
       })
       .catch((error) => {
         console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const getNextPosts = () => {
+    setIsLoading(true);
+    const olderPostsApi = baseApi + "post/older-then";
+    axios
+      .post(olderPostsApi, {
+        date: posts[posts.length - 1].created_at,
+      })
+      .then((response) => {
+        setPosts(posts.concat(response.data));
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
 
@@ -21,7 +47,21 @@ const Home = () => {
     getLatestsPosts();
   }, []);
 
-  return <h2>Home</h2>;
+  return (
+    <div className="home">
+      <div className="postList">
+        {posts.map((post) => {
+          return <Post post={post} key={post.id} />;
+        })}
+        {(isLoading && <Loader />) ||
+          (!isLoading && (
+            <button className="btn loadMore" onClick={getNextPosts}>
+              Load more
+            </button>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
