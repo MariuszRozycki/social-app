80ae1d4039cced618c855511e3b720e8c2c40f1d
MariuszRozycki <mariansc83@icloud.com>
2024-03-12 23:37:02 +0100

    Add props to src/routes/AppRoutes.js

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -3,11 +3,13 @@ import Home from "../views/Home";
 import Login from "../views/Login";
 import SignUp from "../views/SignUp";
 
-const AppRoutes = () => {
+const AppRoutes = (props) => {
+  const { setUser, user } = props;
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login user={user} setUser={setUser} />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
