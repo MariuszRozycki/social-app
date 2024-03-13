97918c1299020ebed7e5a9a23e2e6a9145543046
MariuszRozycki <mariansc83@icloud.com>
2024-03-13 17:57:38 +0100

    Add props user to Home component

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -8,7 +8,7 @@ const AppRoutes = (props) => {
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home user={user} />} />
       <Route path="/login" element={<Login user={user} setUser={setUser} />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
