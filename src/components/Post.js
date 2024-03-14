949a16ba0e745c99dfa1f229f795e4f375ee7753
MariuszRozycki <mariansc83@icloud.com>
2024-03-14 13:49:09 +0100

    Add new props, deletePostBtn, modal, function deletePost

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,33 +1,91 @@
 import "./Post.css";
 import { useState } from "react";
+import axios from "axios";
+import { baseApi } from "../api/baseApi";
 
 const Post = (props) => {
-  console.log(props);
   const {
+    user: { username },
     post: {
-      user: { avatar_url, username },
+      user: { avatar_url, username: postUsername },
       created_at,
       content,
       likes,
+      id,
     },
+    setPosts,
   } = props;
 
   const localDate = new Date(created_at);
   const [likesCount, setLikesCount] = useState(likes.length);
+  const [deleteModalVisible, setDeleteModalVisible] = useState(false);
+
+  const deletePost = (id) => {
+    const deletePostApi = baseApi + "post/delete";
+    console.log(deletePostApi);
+    axios
+      .post(deletePostApi, {
+        post_id: id,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setPosts((posts) => {
+          const updatedPostslist = posts.filter(
+            (post) => post.id !== res.data.post_id
+          );
+          return updatedPostslist;
+        });
+        setDeleteModalVisible(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const deletePostBtn = (
+    <button onClick={() => setDeleteModalVisible(true)} className="btn">
+      Delete
+    </button>
+  );
 
   return (
     <div className="post">
       <div className="avatar">
-        <img src={avatar_url} alt={username} />
+        <img src={avatar_url} alt={postUsername} />
       </div>
       <div className="postData">
         <div className="postMeta">
-          <div className="author">{username}</div>
+          <div className="author">{postUsername}</div>
           <div className="date">{localDate.toLocaleString()}</div>
         </div>
         <div className="postContent">{content}</div>
-        <div className="likes">{likesCount}</div>
+
+        <div className="likes">
+          {postUsername === username && deletePostBtn}
+          {likesCount}
+        </div>
       </div>
+      {deleteModalVisible && (
+        <div className="deleteModal">
+          <h3>Are you sure you want to delete post?</h3>
+          <div className="modalBtnWrapper">
+            <button
+              className="btn yes"
+              onClick={() => {
+                deletePost(id);
+              }}
+            >
+              Yes
+            </button>
+            <button
+              className="btn no"
+              onClick={() => setDeleteModalVisible(false)}
+            >
+              No
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
